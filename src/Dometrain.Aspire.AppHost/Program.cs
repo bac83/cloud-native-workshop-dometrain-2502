eb18bf7111edbdcdbe3b8768ed625d539754776e
Elfocrash <nick.chapsas@gmail.com>
2025-02-27 14:14:31 +0000

    pre redit

--- src/Dometrain.Aspire.AppHost/Program.cs
+++ src/Dometrain.Aspire.AppHost/Program.cs
@@ -6,18 +6,22 @@ var mainDbPassword = builder.AddParameter("postgres-password");
 
 var mainDb = builder.AddPostgres("main-db", mainDbUsername, mainDbPassword, port: 5432)
     .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent)
+    //.WithPgAdmin()
     .AddDatabase("dometrain");
 
 var cartAccount = builder.AddAzureCosmosDB("cosmosdb")
     .AddCosmosDatabase("cartdb");
 
-var cartDb = cartAccount.AddContainer("carts", "/pk");
+cartAccount.AddContainer("carts", "/pk");
+
 
 builder.AddProject<Projects.Dometrain_Monolith_Api>("dometrain-api")
     .WithReference(mainDb)
     .WithReference(cartAccount)
     .WaitFor(cartAccount)
-    .WaitFor(mainDb);
+    .WaitFor(mainDb)
+    .WithReplicas(5);
 
 var app = builder.Build();
     
